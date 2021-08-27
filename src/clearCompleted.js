a09c58c02244bbdcc08d65e430795ee1b2ad7acf
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-08-27 16:05:21 -0700

    Add clearcompleted js file

---
+++ src/clearCompleted.js
@@ -0,0 +1,15 @@
+import showTasks from './showTasks.js';
+import { setLocalStorage } from './setLocalStorage.js';
+import getTasksFromLocalStorage from './getTasks.js';
+// eslint-disable-next-line import/no-cycle
+import setIndex from './index.js';
+// Remove Completed Tasks
+const removeCompletedTasks = () => {
+  let tasks = getTasksFromLocalStorage();
+  tasks = tasks.filter((item) => item.completed === false);
+  setIndex(tasks);
+  setLocalStorage(tasks);
+  showTasks();
+};
+
+export default removeCompletedTasks
